338a9e0b480bbcb43b90c3aa1349f245230d7bbe
rahnerhr <rahnerhr@g.cofc.edu>
2021-09-02 11:49:14 -0400

    Completed Hw#1 Rahner_Hannah

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Hannah Rahner
+hello.py
+
+Problem: print hello, world! to the screen
+
+Certification of Authenticity
+I certify that the assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+main()
